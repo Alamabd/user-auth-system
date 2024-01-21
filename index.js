e517700e490d454fe72af30748ebec2e85f9006d
Alamabd <alamabdillah0108@gmail.com>
2024-01-21 10:12:42 +0700

    login feature

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+require('dotenv').config()
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT
+console.log(port);
+
+app.use(bodyparser.json())
 
 app.use('/', require('./routers'))
+
+// Error Handling
+app.use((req, res, next) => {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send(`Error: ${err.message}`)
+})
+
+
 
 app.listen(port, () => {
     console.log('server running in port http://localhost:3000');
--- routers/login.js
+++ routers/login.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const route = express.Router()
 
-route.get('/', (req, res) => {
-    res.send("Login page")
+route.post('/', (req, res) => {
+    const body = req.body
+    const user = req.locals.user
+
+    // Compare data body and user
+    if(!body.username === user[0].username || !bcrypt.compareSync(body.password, user[0].password)) {
+        return res.status(401).json({ error: "Invalid credentials" })
+    }
+
+    const token = jwt.sign(user[0], process.env.SECRET_KEY);
+    res.json({
+        accesstoken: token
+    })
 })
 
 module.exports = route
