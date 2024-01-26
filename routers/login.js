cad5c2f8aaf891cb30b4d918d4e2aac16a6fd15e
Alamabd <alamabdillah0108@gmail.com>
2024-01-26 21:38:40 +0700

    feat: login

--- routers/login.js
+++ routers/login.js
@@ -1,10 +1,43 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const route = express.Router()
+const pool = require('../utils/db')
+const generateToken = require('../middleware/auth')
 
-route.post('/', (req, res) => {
-    const body = req.body
+/*  
+Request Login Method post
+@body {string} username, password
+*/
+route.post('/', async (req, res) => {
+    const {username, password} = req.body
+
+    try {
+        const query = await pool.query(`SELECT id, username, password FROM public.users WHERE username = '${username}'`)
+
+        // Check a password
+        const match = await bcrypt.compare(password, query.rows[0].password)
+
+        if(match) {
+            res.status(200).json({
+                status: "success",
+                accesstoken: generateToken({
+                    username: username,
+                    password: password
+                }),
+                message: "data is correct"
+            })
+        } else {
+            res.status(404).json({
+                status: "error",
+                message: "not found"
+            })
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: "invalid request"
+        })
+    }
 })
 
 module.exports = route
