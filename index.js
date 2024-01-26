0f15d16f3b20067d5f246f8f678565731e791ef5
Alamabd <alamabdillah0108@gmail.com>
2024-01-26 15:09:38 +0700

    delete: static database

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ const port = process.env.PORT
 
 app.use(bodyparser.json())
 
-// Database
-const user = [
-    // Password 123
-    {id: 1, username: "john", password: "$2b$10$72oFwwNUqNng8/eAAusq2O/jHc8IcaEFIkHuTMhvWG0JW1gJJUkeK"}
-]
-
-// Saving Userto local
-app.use((req, res, next) => {
-    req.locals = req.locals || {}
-    req.locals.user = user
-    next()
-})
-
-
 app.use('/', require('./routers'))
 
 // Error Handling
--- routers/login.js
+++ routers/login.js
@@ -5,12 +5,6 @@ const route = express.Router()
 
 route.post('/', (req, res) => {
     const body = req.body
-    const user = req.locals.user
-
-    // Compare data body and user
-    if(!body.username === user[0].username || !bcrypt.compareSync(body.password, user[0].password)) {
-        return res.status(401).json({ error: "Invalid credentials" })
-    }
 
     const expire = 60 * 60
     // Generate token
