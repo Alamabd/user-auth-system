748addc0455c0049c3d7b5d39bbd0e3e81bb12d4
Alamabd <alamabdillah0108@gmail.com>
2024-01-26 15:20:36 +0700

    delete: request login

--- routers/login.js
+++ routers/login.js
@@ -5,13 +5,6 @@ const route = express.Router()
 
 route.post('/', (req, res) => {
     const body = req.body
-
-    const expire = 60 * 60
-    // Generate token
-    const token = jwt.sign(user[0], process.env.SECRET_KEY, {expiresIn: expire});
-    res.json({
-        accesstoken: token
-    })
 })
 
 module.exports = route
