9bd4bd315e10aee32a0f86fb62fbdb0aed2a2130
Alamabd <alamabdillah0108@gmail.com>
2024-01-28 19:44:38 +0700

    add: verifytoken

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,19 @@ const jwt = require('jsonwebtoken')
 /* Arg {string} data */
 function generateToken(data) {
     const expire = 60 * 60
-    const token = jwt.sign(data, process.env.SECRET_KEY, {expiresIn: expire});
-    return token;
+    const token = jwt.sign(data, process.env.SECRET_KEY, {expiresIn: expire})
+    return token
 }
 
-module.exports = generateToken;
+/* Arg {string} token, {object / boolean} callback */
+function verifyToken(token, callback) {
+    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+        if (err) {
+            callback(false)
+        } else {
+            callback(decoded)
+        }
+    });
+}
+
+module.exports = {generateToken, verifyToken}
