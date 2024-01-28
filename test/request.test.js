1547611e78f257cf7977d69a6ea07f277f785b4a
Alamabd <alamabdillah0108@gmail.com>
2024-01-28 19:00:25 +0700

    add: testing get data

--- test/request.test.js
+++ test/request.test.js
@@ -31,3 +31,17 @@ describe('POST /login', () => {
         expect(res.statusCode).toBe(200)
     })
 })
+
+/* @param {string} accesstoken */
+const accesstoken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG4iLCJwYXNzd29yZCI6IjEyMyIsImlhdCI6MTcwNjQ0MjczOCwiZXhwIjoxNzA2NDQ2MzM4fQ.K5eFlFI_oZm_7DHkGAjGt1tWxQoTv25RBcnd6ArbTJg'
+
+describe('GET /data', () => {
+    it('responds with json', async () => {
+        const res = await request(route)
+            .get(`/data?accesstoken=${accesstoken}`)
+            .set('Accept', 'application/json')
+
+        console.log(res.body);
+        expect(res.statusCode).toBe(200)
+    })
+})
