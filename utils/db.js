ff236a88288941e0e30391ed91a58ba5a0e33cf1
Alamabd <alamabdillah0108@gmail.com>
2024-01-30 09:29:53 +0700

    update: add connectionString

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     user: process.env.DBuser,
     host: process.env.DBhost,
     database: process.env.DBdatabase,
